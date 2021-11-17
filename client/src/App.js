0db0af6a3abfcb10f0aab2873803e99c69942a11
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-11-17 14:05:18 -0800

    updated front-end to get API

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
+import {useState, useEffect} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [api_response, set_api_response] = useState("");
+
+  const call_API = () => {
+    fetch("http://localhost:9000/test-API")
+      .then(res => res.text())
+      .then(res => set_api_response(res));
+  }
+
+  useEffect(() => {
+    call_API();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{api_response}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
