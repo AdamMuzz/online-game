b445395978fee3836b8fe2c25ae15ec2d2ed7d9a
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-06 23:23:09 -0800

    updated frontend to show results of server calls

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [api_response, set_api_response] = useState("");
+  const [api_response, set_api_response] = useState("0");
 
   const call_API = () => {
     fetch("http://localhost:9000/test-API")
@@ -12,23 +12,18 @@ function App() {
       .then(res => set_api_response(res));
   }
 
-  useEffect(() => {
+  /*useEffect(() => {
     call_API();
-  }, []);
+  }, []);*/
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{api_response}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>btn clicked: {api_response} times</p>
+
+        <button onClick={call_API}>Click</button>
       </header>
     </div>
   );
