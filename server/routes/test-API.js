214d8970339e17eee8c60c7e336f77cfefc5b0d4
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-06 23:41:00 -0800

    updated backend sends # of times root path fetched

--- server/routes/test-API.js
+++ server/routes/test-API.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
+var clicks = 0;
+
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    clicks++;
+    data = clicks.toString();
+    res.send(data);
 });
 
 module.exports = router;
