29884eede2e468c3194ae5850b95e9481f4d682c
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-08 23:15:35 -0800

    started client UI work

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [api_response, set_api_response] = useState("0");
-
+  /*const [api_response, set_api_response] = useState("0");
   const call_API = () => {
-    fetch("http://localhost:9000/test-API")
+    fetch("http://192.168.1.98:9000/test-API")
       .then(res => res.text())
       .then(res => set_api_response(res));
-  }
+  }*/
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<p>btn clicked: {api_response} times</p>
+        <button onClick={call_API}>Click</button>*/}
 
-        <p>btn clicked: {api_response} times</p>
+        <div id="Title_Wrapper">
+          <h1>Block.IO</h1>
+        </div>
 
-        <button onClick={call_API}>Click</button>
-      </header>
+        <div id="Name_Input_Wrapper">
+          <p>Name: </p>
+          <p>Play</p>
+        </div>
+
+        <div id="Rules_Wrapper">
+          <p>Rule #1</p>
+          <p>Rule #2</p>
+          <p>Rule #3</p>
+        </div>
     </div>
   );
 }
