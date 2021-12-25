e509a22a802ba8b4c27a1d1ab3627265795ef8ba
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-24 22:03:09 -0800

    initalized game screen as a canvas html el

--- client/src/Game-Screen.js
+++ client/src/Game-Screen.js
@@ -1,11 +1,25 @@
 //Game Screen
+import { useEffect, useRef } from 'react';
 import './Game-Screen.css';
 
 
 function Game_Screen(props) {
+	const canvasRef = useRef(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current
+  	const context = canvas.getContext('2d')
+  	context.fillStyle = '#000000'
+  	context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+  }, []);
+
 	return (
+		
 		<div className='App'>
-			<p className='text'>{`Playing as ${props.name}!`}</p>
+			<div id='header'>
+				<p className='text'>{`Playing as ${props.name}!`}</p>
+			</div>
+			<canvas id='screen' ref={canvasRef}/>
 		</div>
 	);
 }
