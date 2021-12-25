5b72f0480dc2e4113c2cbf8199ee17ff3de2f8cd
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-25 12:17:03 -0800

    allow updating canvas at frame rate of screen

--- client/src/Game-Screen.js
+++ client/src/Game-Screen.js
@@ -1,17 +1,37 @@
 //Game Screen
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Game-Screen.css';
 
 
 function Game_Screen(props) {
+	const [key, set_key] = useState(null);
 	const canvasRef = useRef(null);
+	let frameCount = 0;
+
+	const draw = (ctx, frameCount) => {
+  	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  	ctx.fillStyle = '#000000';
+  	ctx.beginPath();
+  	ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI);
+  	ctx.fill();
+  }
 
 	useEffect(() => {
-		const canvas = canvasRef.current
-  	const context = canvas.getContext('2d')
-  	context.fillStyle = '#000000'
-  	context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-  }, []);
+		const canvas = canvasRef.current;
+  	const context = canvas.getContext('2d');
+		let animationFrameId;
+
+  	const render = () => {
+    	frameCount++
+    	draw(context, frameCount)
+    	animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    
+    return () => {
+    	window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [frameCount]);
 
 	return (
 		
@@ -19,6 +39,7 @@ function Game_Screen(props) {
 			<div id='header'>
 				<p className='text'>{`Playing as ${props.name}!`}</p>
 			</div>
+
 			<canvas id='screen' ref={canvasRef}/>
 		</div>
 	);
