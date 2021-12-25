f6a748ae9530b41fdd85633d4e6a01581342a121
Adam Muzzarelli <admuzzarelli@gmail.com>
2021-12-25 13:56:19 -0800

    added player movement

--- client/src/Game-Screen.js
+++ client/src/Game-Screen.js
@@ -9,6 +9,12 @@ class Player {
 		this.x = 0;
 		this.y = 0;
 	}
+	move(dirs) {
+		if (dirs[0]) {this.y--;}
+		if (dirs[1]) {this.x--;}
+		if (dirs[2]) {this.y++;}
+		if (dirs[3]) {this.x++;}
+	}
 	render(ctx) {
 		ctx.beginPath();
 		ctx.fillStyle = this.color;
@@ -19,10 +25,55 @@ class Player {
 
 function Game_Screen(props) {
 	const [sprites, set_sprites] = useState([]);
-	const [keys, set_keys] = useState([false,false,false,false]);
 	const canvasRef = useRef(null);
+	let dirs = [false,false,false,false];
 	let frameCount = 0;
 	let me = new Player(props.name, '#0f0');
+
+	const handle_key_down = (e) => {
+		switch(e.key) {
+			case 'w':
+				if (!dirs[0]) {dirs[0] = true;}
+				break;
+			case 'a':
+				if (!dirs[1]) {dirs[1] = true;}
+				break;
+			case 's':
+				if (!dirs[2]) {dirs[2] = true;}
+				break;
+			case 'd':
+				if (!dirs[3]) {dirs[3] = true;}
+				break;
+			default:
+				return;
+		}
+	}
+	const handle_key_up = (e) => {
+		switch(e.key) {
+			case 'w':
+				dirs[0] = false;
+				break;
+			case 'a':
+				dirs[1] = false;
+				break;
+			case 's':
+				dirs[2] = false;
+				break;
+			case 'd':
+				dirs[3] = false;
+				break;
+			default:
+				return;
+		}
+	}
+	useEffect(() => {
+		document.addEventListener("keydown", handle_key_down);
+		document.addEventListener("keyup", handle_key_up);
+		return () => {
+			document.removeEventListener("keydown", handle_key_down);
+			document.removeEventListener("keyup", handle_key_up);
+		};
+	}, []);
 
 	const draw = (ctx) => {
 		//clear screen and draw background
@@ -32,6 +83,7 @@ function Game_Screen(props) {
 		ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
 		ctx.fill();
 		//draw player
+		me.move(dirs);
 		me.render(ctx);
 		//draw all other sprites
 		for (let i in sprites) {
