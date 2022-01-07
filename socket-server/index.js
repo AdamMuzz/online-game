6ac72fdb0d42f64da4d253212a3060ed9a44be87
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-06 21:36:18 -0800

    updated backend to support lobbies

--- socket-server/index.js
+++ socket-server/index.js
@@ -10,9 +10,7 @@ const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
-		methods: ['GET', 'POST']
-	}
+	cors: {methods: ['GET', 'POST']}
 });
 
 
@@ -24,63 +22,82 @@ app.get('/', (req,res) => {
 
 
 
-//socket io communication handling
+//backend data structs
 class Player {
-	constructor(id) {
+	constructor(id, lobby) {
 		this.id = id;
 		this.x = 0;
 		this.y = 0;
+		this.lobby = lobby;
 	}
 }
-const players = new Map();
 
+const players = new Map();	//maps socket --> player
+const lobbies = new Map();	//maps lobby name --> set of sockets in said lobby
+
+
+
+//handle socket io communication
 io.on('connection', (socket) => {
 	//handle when player joins
-	socket.on('joined', (id) => {
-		//send all already connected users to player
-		let connected = [];
-		for (const i of players.values()) {
-			connected.push(i);
+	socket.on('joined', (id, lobby) => {
+		//add user to lobby
+		socket.join(lobby);
+		//if lobby user is trying to join exists, send user all already connected players
+		if (lobbies.has(lobby)) {
+			const connected = [];
+			for (const player_socket of lobbies.get(lobby)) {
+				connected.push(players.get(player_socket));
+			}
+			socket.emit('welcome', connected);
 		}
-		socket.emit('welcome', connected);
+		//otherwise create the lobby
+		else {
+			lobbies.set(lobby, new Set());
+		}
 
 		//let all other players know new player joined
-		socket.broadcast.emit('joined', id);
+		socket.to(lobby).emit('joined', id);
 
-		//add new player to connected players
-		players.set(socket, new Player(id));
+		//add new player to connected players, and to lobby
+		players.set(socket, new Player(id, lobby));
+		lobbies.get(lobby).add(socket);
 
-		console.log(`${id} connected`);
+		console.log(`${id} connected to lobby ${lobby}`);
 	});
 
 	//handle when player moves
 	socket.on('moved', (coords) => {
 		//update player's pos
-		let [x,y] = coords;
+		const [x,y] = coords;
 		const player = players.get(socket);
 		player.x = x;
 		player.y = y;
 		//relay to all other users
-		socket.broadcast.emit('moved', player);
+		socket.to(player.lobby).emit('moved', player);
 	});
 
 	//handle when player fires
 	socket.on('fired', (proj_data) => {
-		socket.broadcast.emit('fired', proj_data);
+		socket.to(players.get(socket).lobby).emit('fired', proj_data);
 	});
 
 	//handle player dies
 	socket.on('died', (killer_id) => {
-		//[killer_id,killed_id]
-		io.emit('died', [killer_id, players.get(socket).id]);
+		const player = players.get(socket);
+		io.to(player.lobby).emit('died', [killer_id, player.id]);
 	});
 
 	//handle when player disconnects
 	socket.on('disconnect', () => {
-		id = players.get(socket).id;
+		const player = players.get(socket);
 		players.delete(socket);
-		socket.broadcast.emit('left', id);
-		console.log(`${id} has left the game`);
+		lobbies.get(player.lobby).delete(socket);
+		socket.to(player.lobby).emit('left', player.id);
+
+		if (lobbies.get(player.lobby).size == 0) {lobbies.delete(player.lobby);}
+
+		console.log(`${player.id} has left the game`);
 	});
 });
 
